3e0762091c49b3afe3a07b12534c7c20d5fba017
Rakshit Kumar <32777813+mighty-phoenix@users.noreply.github.com>
2019-11-23 13:56:26 +0530

    Update settings.py

--- split/settings.py
+++ split/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = 'b($1gf=^x6r-*fn^3-26^@_1^qnodrrjd6-9b3arctx@pf)@ah'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['tap-search-1.herokuapp.com']
 MEDIA_URL = 'splitter/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'splitter/media')
 
@@ -109,3 +109,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+import django_heroku
+django_heroku.settings(locals())
